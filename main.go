8bf2ea33dc5d2a383512e8a80eaa479d34312147
George Giatrakos <geo.giatrakos@gmail.com>
2021-07-26 20:38:22 +0300

    First Commit
    + Implemented first simple listener

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
   "fmt"
+  "net"
 )
 
+func handleConnection(conn net.Conn ) {
+  adr := conn.RemoteAddr()
+  fmt.Println("Handling conenction type "+adr.Network()+" from " + adr.String())
+  fmt.Print("")
+  conn.Close()
+
+}
 
 // Main function
 func main() {
 
     fmt.Println("Hello World")
+
+    ln, err := net.Listen("tcp", ":8080")
+    if err != nil {
+    	// handle error
+    }
+    for {
+    	conn, err := ln.Accept()
+    	if err != nil {
+    		// handle error
+    	}
+    	go handleConnection(conn)
+    }
 
 }
